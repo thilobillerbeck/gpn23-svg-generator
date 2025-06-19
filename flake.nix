d0e2f61de232a8134b55ba08d1009d87b9096245
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2025-06-19 11:50:14 +0200

    add minimal flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        let
+          nodejs = pkgs.nodejs_22;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              nodejs
+            ];
+          };
+        }
+      );
+    };
+}
